eb3d16e0a82af4d96637ea6b78124c851edcbec0
ahlersml <70450921+ahlersml@users.noreply.github.com>
2023-09-15 17:20:47 -0400

    Banner Layout Created (forgot to use branch)

--- app/src/main/java/com/example/seniordesignproject/MainActivity.kt
+++ app/src/main/java/com/example/seniordesignproject/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.seniordesignproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.seniordesignproject.ui.theme.SeniorDesignProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +35,9 @@ class MainActivity : ComponentActivity() {
             SeniorDesignProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BuildHomePage()
                 }
             }
         }
@@ -30,17 +45,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SeniorDesignProjectTheme {
-        Greeting("Android")
+fun BuildHomePage() {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(LocalConfiguration.current.screenWidthDp.dp/6)
+    ){
+        Banner()
     }
+
+}
+@Composable
+fun Banner(){
+
+    Column(
+        Modifier
+            .width(LocalConfiguration.current.screenWidthDp.dp/6)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "logo",
+            modifier = Modifier
+                .padding(all = 2.dp)
+
+            )
+    }
+
+    Column {
+        Text(
+            text = "APP NAME HERE",
+            //add modifications to banner text here
+        )
+    }
+
+    Column (
+        Modifier
+            .width(LocalConfiguration.current.screenWidthDp.dp/6)
+    ){
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(color= Color.Blue)
+        )
+        //ADD menu button here
+    }
+
 }
