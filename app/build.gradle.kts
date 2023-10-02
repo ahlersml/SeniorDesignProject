87520e37155c58a7aa2fcf7c6cd58334e62bf60b
Alex Hedges <hedgesaj@mail.uc.edu>
2023-10-02 18:45:49 -0400

    Updated dependencies to include Crashlytics and performance monitoring.
    Main activity was updated to initialize Firebase with Google Play App Check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    //Google Services plugin
     id("com.google.gms.google-services")
+
+    //Crashlytics plugin
+    id("com.google.firebase.crashlytics")
+
+    //Performance Monitoring
+    id("com.google.firebase.firebase-perf")
+
 }
 
 android {
@@ -72,4 +81,15 @@ dependencies {
     //dependencies needed for Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-analytics-ktx")
+
+    //depencies needed for Google Play App Check
+    implementation("com.google.firebase:firebase-appcheck-playintegrity")
+    implementation("com.google.firebase:firebase-appcheck-ktx")
+
+    //dependencies needed for Crashlytics
+    implementation("com.google.firebase:firebase-crashlytics-ktx")
+    implementation("com.google.firebase:firebase-analytics-ktx")
+
+    //dependencies needed for performance monitoring
+    implementation("com.google.firebase:firebase-perf-ktx")
 }
